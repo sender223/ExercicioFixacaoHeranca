1e0abfcbf72c58f23b438a7a2864e4f5b3d39b41
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2019-10-08 15:52:56 -0300

    Add project files.

---
+++ ExercicioFixacaoHeranca/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExercicioFixacaoHeranca {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
